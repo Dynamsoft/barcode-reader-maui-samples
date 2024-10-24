c19fa4969052b48b6c3f9b2d38bd98d465b691b0
cjpyxp <40759083+cjpyxp@users.noreply.github.com>
2024-10-24 17:46:43 +0800

    update to 10.4.2000

---
+++ BarcodeReaderSimpleSample/BarcodeReaderSimpleSample/CameraPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using Dynamsoft.Core.Maui;
+using Dynamsoft.CaptureVisionRouter.Maui;
+using Dynamsoft.BarcodeReader.Maui;
+using Dynamsoft.CameraEnhancer.Maui;
+
+namespace BarcodeReaderSimpleSample;
+
+public partial class CameraPage : ContentPage, ICapturedResultReceiver, ICompletionListener
+{
+    public static CameraEnhancer enhancer;
+    CaptureVisionRouter router;
+
+    public CameraPage()
+    {
+        InitializeComponent();
+        enhancer = new CameraEnhancer();
+        router = new CaptureVisionRouter();
+        router.SetInput(enhancer);
+        router.AddResultReceiver(this);
+    }
+
+    protected override void OnHandlerChanged()
+    {
+        base.OnHandlerChanged();
+        if (this.Handler != null)
+        {
+            enhancer.SetCameraView(camera);
+        }
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        await Permissions.RequestAsync<Permissions.Camera>();
+        enhancer?.Open();
+        router?.StartCapturing(EnumPresetTemplate.PT_READ_SINGLE_BARCODE, this);
+    }
+
+    protected override void OnDisappearing()
+    {
+        base.OnDisappearing();
+        enhancer?.Close();
+        router?.StopCapturing();
+    }
+
+    public void OnDecodedBarcodesReceived(DecodedBarcodesResult result)
+    {
+        if (result != null && result.Items != null && result.Items.Count > 0)
+        {
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                router?.StopCapturing();
+                enhancer?.ClearBuffer();
+            });
+            var message = "";
+            foreach (var item in result.Items)
+            {
+                message += "\nFormat: " + item.FormatString + "\nText: " + item.Text + "\n";
+            }
+            MainThread.BeginInvokeOnMainThread(async () =>
+            {
+                await DisplayAlert("Results", message, "OK");
+                router?.StartCapturing(EnumPresetTemplate.PT_READ_SINGLE_BARCODE, this);
+            });
+        }
+    }
+
+    public void OnFailure(int errorCode, string errorMessage)
+    {
+        MainThread.BeginInvokeOnMainThread(() =>
+        {
+            DisplayAlert("Error", errorMessage, "OK");
+        });
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# barcode-reader-maui-samples
+# Dynamsoft Barcode Reader samples for MAUI edition
+
+This repository contains multiple samples that demonstrate how to use the [Dynamsoft Barcode Reader MAUI SDK](https://www.dynamsoft.com/barcode-reader/docs/mobile/programming/maui/).
+
+- [User Guide](https://www.dynamsoft.com/barcode-reader/docs/mobile/programming/maui/user-guide.html)
+- [API Reference](https://www.dynamsoft.com/barcode-reader/docs/mobile/programming/maui/api-reference/)
+  
+## System Requirements
+
+### .Net
+
+- .NET 7.0 and above.
+
+### Android
+
+- Supported OS: **Android 5.0** (API Level 21) or higher.
+- Supported ABI: **armeabi-v7a**, **arm64-v8a**, **x86** and **x86_64**.
+- Development Environment: Visual Studio 2022.
+- JDK: 1.8+
+
+### iOS
+
+- Supported OS: **iOS 11.0** or higher.
+- Supported ABI: **arm64** and **x86_64**.
+- Development Environment: Visual Studio 2022 for Mac and Xcode 14.3+.
+
+## Samples
+
+| Sample Name | Description |
+| ----------- | ----------- |
+| `BarcodeReaderSimpleSample` | This is a sample that illustrates the simplest way to recognize barcodes from video streaming. |
+
+## Installation
+
+- [Install the Dynamsoft Barcode Reader MAUI SDK](https://www.dynamsoft.com/barcode-reader/docs/mobile/programming/maui/user-guide.html#installation)
+
+## License
+
+- You can request a 30-day trial license via the [Request a Trial License](https://www.dynamsoft.com/customer/license/trialLicense?product=dbr&utm_source=samples&package=mobile) link.
+
+## Contact Us
+
+For any questions or feedback, you can either [contact us](https://www.dynamsoft.com/company/contact/) or [submit an issue](https://github.com/Dynamsoft/barcode-reader-maui-samples/issues/new).
