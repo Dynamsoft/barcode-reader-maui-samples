071a03f6b9a17e11997d1ce49feaa9dd67868a13
DMGithubPublisher <github@dynamsoft.com>
2025-08-12 14:55:02 +0800

    update to 11.0.5100

--- ScanBarcodes_ReadyToUseComponent/MainPage.xaml.cs
+++ ScanBarcodes_ReadyToUseComponent/MainPage.xaml.cs
@@ -8,7 +8,7 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
-	private async void OnScanBarcode(object sender, EventArgs e)
+	private async void OnScanSingleBarcode(object sender, EventArgs e)
     {
 		// Initialize the license.
         // The license string here is a trial license. Note that network connection is required for this license to work.
@@ -18,7 +18,27 @@ public partial class MainPage : ContentPage
         var message = result.ResultStatus switch
         {
             EnumResultStatus.Finished => string.Join("\n",
-                result.Barcodes!.Select(item => item.FormatString + "\n" + item.Text)),
+                result.Barcodes!.Select(item => "Format: "+item.FormatString + "\nText: " + item.Text)),
+            EnumResultStatus.Canceled => "Scanning canceled",
+            EnumResultStatus.Exception => result.ErrorString,
+            _ => throw new ArgumentOutOfRangeException()
+        };
+		label.Text = message;
+    }
+	
+	private async void OnScanMultipleBarcode(object sender, EventArgs e)
+    {
+		// Initialize the license.
+        // The license string here is a trial license. Note that network connection is required for this license to work.
+        // You can request an extension via the following link: https://www.dynamsoft.com/customer/license/trialLicense?product=dbr&utm_source=samples&package=mobile
+        var config = new BarcodeScannerConfig("DLS2eyJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSJ9");
+        config.ScanningMode = EnumScanningMode.Multiple;
+        var result = await BarcodeScanner.Start(config);
+        var message = result.ResultStatus switch
+        {
+            EnumResultStatus.Finished =>
+	            "Barcodes Count: "+result.Barcodes?.Length + "\n\n" +
+	            string.Join("\n\n", result.Barcodes!.Select(item => "Format: "+item.FormatString + "\nText:" + item.Text)),
             EnumResultStatus.Canceled => "Scanning canceled",
             EnumResultStatus.Exception => result.ErrorString,
             _ => throw new ArgumentOutOfRangeException()
